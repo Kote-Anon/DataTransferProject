df76aadf45a9c3dae00584b6913b27918656cba0
Kote-Anon <antonibedkowski@gmail.com>
2024-04-16 21:36:14 +0200

    testing UI library

--- DTPServer/main.cpp
+++ DTPServer/main.cpp
@@ -1,8 +1,29 @@
 // main.cpp
 
 #include "serverHandler.cpp"
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
 
-int main() {
+int main(void) {
+    using namespace ftxui;
+
+    //define document
+    Element document =
+    hbox({
+      text("left")   | border,
+      text("middle") | border | flex,
+      text("right")  | border,
+    });
+ 
+    auto screen = Screen::Create(
+    Dimension::Full(),       // Width
+    Dimension::Fit(document) // Height
+    );
+    Render(screen, document);
+    screen.Print();
+ 
+    return EXIT_SUCCESS;
+
     runServer();
     return 0;
 }
