69eb1bcc17e6542b716b301cdc172dfc08e14e82
Kote-Anon <antonibedkowski@gmail.com>
2024-04-16 21:38:05 +0200

    deleting ui changes

--- DTPServer/main.cpp
+++ DTPServer/main.cpp
@@ -1,29 +1,8 @@
 // main.cpp
 
 #include "serverHandler.cpp"
-#include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/screen.hpp>
 
-int main(void) {
-    using namespace ftxui;
-
-    //define document
-    Element document =
-    hbox({
-      text("left")   | border,
-      text("middle") | border | flex,
-      text("right")  | border,
-    });
- 
-    auto screen = Screen::Create(
-    Dimension::Full(),       // Width
-    Dimension::Fit(document) // Height
-    );
-    Render(screen, document);
-    screen.Print();
- 
-    return EXIT_SUCCESS;
-
+int main() {
     runServer();
     return 0;
 }
